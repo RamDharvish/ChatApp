1b5d729050d15393e7e834f761ba40a45b7ee089
RamDharvish <rdharvish@gmail.com>
2023-07-18 12:00:09 +0530

    first commit

---
+++ src/Components/NetFlix/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from './NavBar'
+import "./Main.css"
+import Banner from './Banner'
+import RowPost from './RowPost'
+function Main() {
+  return (
+    <div>
+        <NavBar/>
+        <Banner/>
+        <RowPost/>
+    </div>
+  )
+}
+
+export default Main
